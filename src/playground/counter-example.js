ecef99d4a29569bc95298c2b56773e2fce9a5205
Manav Bokinala <manav.bokinala@gmail.com>
2017-12-03 09:08:46 -0800

    Minor bug fixes and added local storage of count in counter-example.js

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -7,8 +7,23 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: props.count
+            count: 0
         };
+    }
+
+    componentDidMount() {
+        const ls = localStorage.getItem('count');
+        const count = parseInt(ls, 10);
+
+        if(!isNaN(count)) {
+            this.setState(() => ({count}));
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(this.state.count != prevState.count)  {
+            localStorage.setItem('count', this.state.count);
+        }
     }
 
     handleAddOne() {
@@ -45,10 +60,6 @@ class Counter extends React.Component {
             </div>
         );
     }
-}
-
-Counter.defaultProps = {
-    count: 0
 }
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
