a9d7e07e6f1ea71fb840deb95f91d56ecb9d32d7
Manav Bokinala <manav.bokinala@gmail.com>
2017-12-25 19:29:22 -0800

    Set up scss

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -18,12 +18,11 @@ export default class AddOption extends React.Component {
   render() {
     return (
       <div>
-        {this.state.error && <p>{this.state.error}</p>}<form
-        onSubmit={this.handleAddOption}
-      >
-        <input type="text" name="option" />
-        <button> Add Option </button>
-      </form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button> Add Option </button>
+        </form>
       </div>
     );
   }
