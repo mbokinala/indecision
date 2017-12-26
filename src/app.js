80fb7b4b63ed6685bd7e189d6c2d23b4a3feb5f6
Manav Bokinala <manav.bokinala@gmail.com>
2017-12-25 17:42:50 -0800

    Deleted old playground things

--- src/app.js
+++ src/app.js
@@ -3,12 +3,3 @@ import ReactDOM from "react-dom";
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
-
-class OldSyntax {
-    constructor() {
-        this.name = 'Manav';
-    }
-}
-
-const oldSyntax = new OldSyntax();
-console.log(oldSyntax);
