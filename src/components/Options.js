3ed1f148ed16a377f32faa97f83b0036e2acb1b8
Manav Bokinala <manav.bokinala@gmail.com>
2017-12-26 10:25:14 -0800

    Styled widget

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,15 +3,18 @@ import Option from './Option';
 
 const Options = props => (
   <div>
-    <button 
-      className="button button--link"
-      onClick={props.handleDeleteOptions}>
-        Remove All
-    </button>
+    <div className="widget--header">
+      <h3 className="widget-header__title ">Your Options</h3>
+      <button
+        className="button button--link"
+        onClick={props.handleDeleteOptions}>
+          Remove All
+      </button>
+    </div>
 
     {props.options.length === 0 && (
       <div>
-        <em>Please add an option to get started</em>
+        <p className="widget__message">Please add an option to get started</p>
       </div>
     )}
     
